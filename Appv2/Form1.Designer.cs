f15f6694fd73083b04aeac088a3c58c365eb595a
Zetcie <zofia.cieplucha@wp.pl>
2019-08-28 20:28:26 +0200

    28.08.2019

---
+++ Appv2/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Appv2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            resources.GetString("comboBox1.Items"),
+            resources.GetString("comboBox1.Items1"),
+            resources.GetString("comboBox1.Items2"),
+            resources.GetString("comboBox1.Items3")});
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.SelectionChangeCommitted += new System.EventHandler(this.comboBox1_SelectionChangeCommitted);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            resources.ApplyResources(this.button5, "button5");
+            this.button5.Name = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // videoSourcePlayer1
+            // 
+            resources.ApplyResources(this.videoSourcePlayer1, "videoSourcePlayer1");
+            this.videoSourcePlayer1.Name = "videoSourcePlayer1";
+            this.videoSourcePlayer1.VideoSource = null;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.videoSourcePlayer1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
+    }
+}
+
---
+++ Appv2/Form1.cs
@@ -0,0 +1,143 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+
+namespace Appv2
+{
+    public partial class Form1 : Form
+    {
+
+        string fileContent = string.Empty;
+        string filePath = string.Empty;
+        Image zmienna = new Bitmap("C:/Users/Zosienka/Desktop/semestr6/GiM/znaki/zielony.png");
+        PointF ulCorner = new PointF(550.0F, 25.0F);
+        //private static string zmienna;
+        FileVideoSource videoSource = new FileVideoSource(@"C:\Users\Zosienka\Desktop\semestr6\GiM\GiM-master\film1.avi");
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //start filmu
+        private void button1_Click(object sender, EventArgs e)
+        {
+            videoSource.Start();
+            videoSourcePlayer1.VideoSource = videoSource;
+        }
+
+        //ładowanie
+        private void button5_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "avi files (*.avi)|*.avi|All files (*.*)|*.*";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                
+
+                FileVideoSource videoSource = new FileVideoSource(openFileDialog.FileName);
+                //videoSource.NewFrame += new NewFrameEventHandler(video_NewFrame);
+                videoSource.Start();
+                videoSourcePlayer1.VideoSource = videoSource;
+
+            }
+        }
+
+
+        public void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bitmap = eventArgs.Frame;
+            Graphics graphics = Graphics.FromImage(bitmap);
+            graphics.DrawImage(SetImageOpacity(zmienna, 0.5f), ulCorner); //znak wodny
+
+            /*SolidBrush alphaBrush = new SolidBrush(Color.FromArgb(128, 255, 0, 0));
+            Font f = new Font(FontFamily.GenericSerif, 30.0f, FontStyle.Bold);
+            
+            graphics.DrawString("chuj", f, alphaBrush, 50, 50);//napis*/
+            
+
+        }
+
+        public Image SetImageOpacity(Image image, float opacity)
+        {
+            Bitmap bmp = new Bitmap(80, 80);
+
+            using (Graphics gfx = Graphics.FromImage(bmp))
+            {
+                ColorMatrix matrix = new ColorMatrix();
+
+                matrix.Matrix33 = opacity;
+
+                ImageAttributes attributes = new ImageAttributes();
+
+                attributes.SetColorMatrix(matrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
+
+                gfx.DrawImage(image, new Rectangle(0, 0, bmp.Width, bmp.Height), 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, attributes);
+            }
+            return bmp;
+        }   
+
+        public void comboBox1_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+
+            ComboBox senderComboBox = (ComboBox)sender;
+            int selectedIndex = comboBox1.SelectedIndex;
+            Object selectedItem = comboBox1.SelectedItem;
+
+            if(selectedIndex.Equals(0))
+            {                
+                zmienna = new Bitmap("C:/Users/Zosienka/Desktop/semestr6/GiM/znaki/zielony.png");
+            }
+            else if(selectedIndex.Equals(1))
+            {
+                zmienna = new Bitmap("C:/Users/Zosienka/Desktop/semestr6/GiM/znaki/zolty.png");
+            }
+            else if (selectedIndex.Equals(2))
+            {
+                zmienna = new Bitmap("C:/Users/Zosienka/Desktop/semestr6/GiM/znaki/pomaranczowy.png");
+            }
+            else if (selectedIndex.Equals(3))
+            {
+                zmienna = new Bitmap("C:/Users/Zosienka/Desktop/semestr6/GiM/znaki/czerwony.png");
+            }
+
+            //MessageBox.Show("Selected Item Text: " + selectedItem.ToString() + "\n" +
+            //"Index: " + selectedIndex.ToString());
+        }
+        public void button2_Click(object sender, EventArgs e)
+        {
+            videoSource.NewFrame += new NewFrameEventHandler(video_NewFrame);
+            videoSourcePlayer1.VideoSource = videoSource;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            videoSource.NewFrame -= new NewFrameEventHandler(video_NewFrame);
+            videoSourcePlayer1.VideoSource = videoSource;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            videoSource.Stop();
+        }
+
+    
+    }
+
+}
